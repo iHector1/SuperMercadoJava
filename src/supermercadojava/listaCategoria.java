84e9bd3a52dbf9432cadf8358e62b5901e843953
HECTOR JOSUE GONZALEZ CORTES <62352013+iHector1@users.noreply.github.com>
2022-06-12 14:44:44 -0500

    lsita de categoria completadoooo

--- src/supermercadojava/listaCategoria.java
+++ src/supermercadojava/listaCategoria.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package supermercadojava;
 
-/**
- *
- * @author PC
- */
 public class listaCategoria {
-    
+    private nodoCategoria inicio;
+
+    public listaCategoria() {
+        this.inicio = null;
+    }
+    public void agregar(Categoria categoria){
+        nodoCategoria nodo=new nodoCategoria(categoria);
+        if(this.inicio==null){
+            this.inicio=nodo;
+        }else{
+            nodoCategoria aux=inicio;
+            while(aux.getNodo()!=null){
+                aux=aux.getNodo();
+            }
+            aux.setNodo(nodo);
+        }
+    }
 }
--- src/supermercadojava/nodoCategoria.java
+++ src/supermercadojava/nodoCategoria.java
@@ -10,5 +10,28 @@ package supermercadojava;
  * @author PC
  */
 public class nodoCategoria {
+    private Categoria categoria;
+    private nodoCategoria nodo;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public nodoCategoria getNodo() {
+        return nodo;
+    }
+
+    public void setNodo(nodoCategoria nodo) {
+        this.nodo = nodo;
+    }
+
+    public nodoCategoria(Categoria categoria) {
+        this.categoria = categoria;
+        this.nodo = null;
+    }
     
 }
