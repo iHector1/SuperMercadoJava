dbe5d9765a22d8aae4484157046f34ec4503c2b9
HECTOR JOSUE GONZALEZ CORTES <62352013+iHector1@users.noreply.github.com>
2022-06-13 12:26:43 -0500

    base de datos hecha

--- src/supermercadojava/Datos.java
+++ src/supermercadojava/Datos.java
@@ -12,10 +12,14 @@ public class Datos {
        this.admis=new listaAdministradores();
        this.productos=new listaProducto();
        this.categoria=new listaCategoria();
+       this.init();
     }
     
     private void init(){
-       // Empleado em=new Empleado();
+        Empleado em=new Empleado("12","7am a 5pm",2500,"Pasillos","1234","David"
+                ,"Loera","12/01/2001",20,"Nueva Escocia");
+        Administrador ad=new Administrador("1","Oficinas","7am a 7pm",5000,
+                "12/10/2010","4321","Benja","Cortina","2/09/1985",30,"Pablo neruda");
     }
     
     
